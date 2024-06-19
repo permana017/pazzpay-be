3e407daacbe1e65c12536776c387448c9b5ae4ed
permana017 <ahdipermana07@gmail.com>
2024-06-19 13:25:42 +0700

    change description root route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express();
 
 const authRoute = require("./auth.route");
-const usersRoute = require('./users.route');
-const transferRoute = require('./transfer.route');
+const usersRoute = require("./users.route");
+const transferRoute = require("./transfer.route");
 
-router.get('/', (req, res) => {
-    return res.send('Backend for Hiring Job App - ARRAY')
-})
+router.get("/", (req, res) => {
+  return res.send("Backend for Pazzpay");
+});
 
 router.use("/transaction", transferRoute);
 router.use("/auth", authRoute);
-router.use('/users', usersRoute);
+router.use("/users", usersRoute);
 module.exports = router;
